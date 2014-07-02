650e6afe30c7d61c3dbbf1b99bcda163da1611a9
BigK1337 <kyleahicks@yahoo.com>
2014-07-02 16:22:10 -0400

    Answers to Quiz
    Important Note:
    Question 1 is under Quiz
    Question 2 is under ConsoleApplication2
    Question 3 is under ConsoleApplication3

---
+++ Quiz/ConsoleApplication2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double book1 = 5.75;
+            double book2 = 8.00;
+            int k;
+            Console.WriteLine("Hitchhikers Guide to the Galaxy is: $" + book1);
+            Console.WriteLine("Dune is: $" + book2);
+            if (book1 < book2)
+            {
+                k = -1;
+                Console.Write(k);
+            }
+            else if (book1 > book2)
+            {
+                k = 1;
+                Console.Write(k);
+            }
+            else
+            {
+                k = 0;
+                Console.Write(k);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Program
+    {
+        public void Compare (int a, int b)
+        {
+            if ( a < b)
+            {
+                Console.WriteLine(a + " is the smallest number");
+            }
+
+            else
+            {
+                Console.WriteLine(b + " is the smallest number");
+            }
+        }
+        static void Main(string[] args)
+        {
+            for(int z = 0; z <1000; z++)
+            {
+                int x; 
+            int y;
+            Console.WriteLine("Enter first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Enter second number: ");
+             y = Convert.ToInt32(Console.ReadLine());
+             
+                if (x < y)
+                {
+                    Console.WriteLine(x + " is the smallest number.");
+                }
+                else
+                {
+                    Console.WriteLine(y + " is the smallest number.");
+                }
+            }
+        }
+    }
+}
